68a51469f973f7ba2961c0511caea4f53bb89a59
kimkijun0226 <kimkijun0226@gmail.com>
2025-06-22 17:06:15 +0900

    학습: 기본 컴포넌트

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,47 @@
-import { View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function HomeScreen() {
-  return <View></View>;
+  const [text, setText] = useState("");
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>123</Text>
+      </View>
+
+      <TextInput
+        value={text}
+        onChangeText={(value) => setText(value)}
+        style={styles.input}
+      />
+
+      <Button
+        title="버튼 이름"
+        onPress={() => {
+          console.log("pressed!");
+        }}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "yellow",
+  },
+  text: {
+    color: "red",
+    fontSize: 30,
+  },
+  input: {
+    fontSize: 30,
+  },
+});
